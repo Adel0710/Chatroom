8e9203fab417804731638352c7de796a5177ad6e
cisse_j <cisse_j@etna-alternance.net>
2024-04-05 13:10:12 +0000

    con & reg first step

---
+++ controller/user/RegisterController.ts
@@ -0,0 +1,23 @@
+import { pool } from '../../model/dbConnect'
+import bcrypt from 'bcrypt'
+
+export default async function registerController(username: any,password: any,firstName: string,lastName: string,email: any,res:any) {
+    console.log('good2');
+    
+    try {
+        console.log('good3');
+        
+        console.log(username, password, firstName, lastName, email);
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+        let requete_sql =`INSERT INTO users(login, mdp, email, date_inscription,nom, prenom) VALUES (?,?,?,NOW(),?,?)`;
+        var inserts = [username, hashedPassword, email, lastName, firstName ];
+        
+        await pool.execute(requete_sql, inserts);
+        console.log('vous avez été inscrit');
+        res.redirect('/')
+    } catch (error) {
+        console.error("Vous n'avez pas été inscrit, recommencez");
+        
+    }
+}
--- model/dbConnect.ts
+++ model/dbConnect.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql2'
 import { config } from './config';
 
-export const pool = mysql.createPool({
+export const pool = mysql.createConnection({
     host: config.host,
     user: config.user,
     password:config.password,
